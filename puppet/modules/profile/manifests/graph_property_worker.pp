2e1c27a9baf48c37853683f4dd8f4850a6c37f54
Gordon Shankman <gordon.shankman@altamiracorp.com>
2015-05-22 14:38:35 -0400

    Adding ignored profile directory.

---
+++ puppet/modules/profile/manifests/graph_property_worker.pp
@@ -0,0 +1,11 @@
+# Defines the packages required for a node that will run the
+# GraphPropertyWorker processes.
+class profile::graph_property_worker {
+  class { '::java': }
+  class { '::jai': }
+  class { '::jai_imageio': }
+  class { '::opencv': }
+  class { '::tesseract': }
+  class { '::sphinx': }
+  class { '::ccextractor': }
+}
