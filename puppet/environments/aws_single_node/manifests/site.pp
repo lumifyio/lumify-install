77ae6b6bdbd5564554789410319aaba653ce3ec8
Thomas Hampton <thampton@solutionstreet.com>
2015-06-26 15:18:25 -0400

    created as_single_node environment & config

---
+++ puppet/environments/aws_single_node/manifests/site.pp
@@ -0,0 +1,41 @@
+# management node 1
+node 'elasticip' {
+  class { '::sysctl::max_file_descriptors': } ->
+  class { '::sysctl::max_map_count': } ->
+  class { '::sysctl::swappiness': } ->
+  class { '::iptables': } ->
+  class { '::ipv6': } ->
+  class { '::epel': } ->
+  class { '::jai': } ->
+  class { '::jai_imageio': } ->
+  class { '::opencv': } ->
+  class { '::tesseract': } ->
+  class { '::sphinx': } ->
+  class { '::ccextractor': } ->
+  class { '::ffmpeg': } ->
+  class { '::clavin': } ->
+  class { '::erlang': } ->
+  class { '::rabbitmq': } ->
+  class { '::lumify::config::config': } ->
+  class { '::cloudera::cdh5::zookeeper::zookeeper': } ->
+  class { '::cloudera::cdh5::hadoop::yarn::nodemanager': } ->
+  class { '::cloudera::cdh5::hadoop::yarn::resourcemanager': } ->
+  class { '::cloudera::cdh5::hadoop::yarn::historyserver': } ->
+  class { '::cloudera::cdh5::hadoop::datanode': } ->
+  class { '::cloudera::cdh5::hadoop::namenode': } ->
+  class { '::accumulo::tablet': } ->
+  class { '::accumulo::master': } ->
+  class { '::accumulo::monitor': } ->
+  class { '::accumulo::tracer': } ->
+  class { '::accumulo::gc': } ->
+  class { '::elasticsearch': } ->
+  class { '::elasticsearch_head_plugin': } ->
+  class { '::elasticsearch_hq_plugin': } ->
+  class { '::elasticsearch_securegraph_plugin': } ->
+  class { '::lumify::tools::deploy': } ->
+  class { '::lumify::gpw::deploy': } ->
+  class { '::lumify::ontology::deploy': } ->
+  class { '::lumify::yarn::deploy': } ->
+  class { '::lumify::yarn::run': } ->
+  class { '::lumify::web::deploy': }
+}
