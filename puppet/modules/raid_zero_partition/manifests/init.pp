9bd1201feb648614b8740ecd0ce49ee62f76a1bb
Thomas Hampton <thampton@solutionstreet.com>
2015-07-15 15:23:04 -0400

    fixed mount dir parameter

--- puppet/modules/raid_zero_partition/manifests/init.pp
+++ puppet/modules/raid_zero_partition/manifests/init.pp
@@ -1,6 +1,6 @@
 class raid_zero_partition (
-  $drives     = $raid_zero_partition::drives,
-  $mount_dir  = $raid_zero_partition::mount_dir,
+  $drives     = $raid_zero_partition::params::drives,
+  $mount_dir  = $raid_zero_partition::params::mount_dir,
 ) inherits raid_zero_partition::params {
   class { '::raid_zero_partition::install': }
   contain '::raid_zero_partition::install'
