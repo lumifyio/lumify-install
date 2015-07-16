6fafc4674839fc868b8a49d4ad946f279fcfcc3f
Thomas Hampton <thampton@solutionstreet.com>
2015-07-16 12:36:23 -0400

    Fixed source and target dirs

--- puppet/modules/lumify/manifests/ontology/deploy.pp
+++ puppet/modules/lumify/manifests/ontology/deploy.pp
@@ -2,16 +2,16 @@ class lumify::ontology::deploy inherits lumify {
 
   include '::macro'
 
-  macro::ensure_dir{ "$lumify_ontologies_dir/minimal" :
+  macro::ensure_dir{ "$target_ontologies_dir/minimal" :
     owner   => 'lumify',
     group   => 'lumify',
     mode    => 'u=rx,go=rx',
   }
 
-  file { "$lumify_ontologies_dir/minimal":
-    source  => "$target_ontologies_dir/ontology-minimal",
+  file { "$target_ontologies_dir/minimal":
+    source  => "$lumify_ontologies_dir/ontology-minimal",
     recurse => true,
-    require => Macro::Ensure_dir['/opt/lumify/ontologies/minimal'],
+    require => Macro::Ensure_dir["$target_ontologies_dir/minimal"],
     owner   => 'lumify',
     group   => 'lumify',
     mode    => 'u=rx,go=rx',
