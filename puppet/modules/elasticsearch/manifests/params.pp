7d855aced03c08f43b0673649ae1c5ce7362a7a0
Thomas Hampton <thampton@solutionstreet.com>
2015-07-16 12:35:18 -0400

    Added indices field data cache size param

--- puppet/modules/elasticsearch/manifests/params.pp
+++ puppet/modules/elasticsearch/manifests/params.pp
@@ -1,21 +1,22 @@
 class elasticsearch::params {
-  $elasticsearch_version      = '1.4.1'
-  $elasticsearch_rpm_version  = '1.4.1-1'
-  $user                       = 'elasticsearch'
-  $group                      = 'elasticsearch'
-  $install_dir                = '/usr/share'
-  $config_dir                 = '/etc/elasticsearch'
-  $data_dirs                  = [ "/var/lib/elasticsearch", ]
-  $log_dir                    = '/var/log/elasticsearch'
-  $plugin_dir                 = '/usr/share/elasticsearch/plugins'
-  $work_dir                   = '/tmp/elasticsearch/work'
-  $tmp_dir                    = '/tmp/elasticsearch/tmp'
-  $number_of_shards           = '1'
-  $number_of_replicates       = '0'
-  $es_heap_size               = '2g'
-  $max_open_file_descriptors  = '32768'
-  $mlockall                   = 'true'
-  $cluster_name               = 'lumify_elasticsearch'
+  $elasticsearch_version        = '1.4.1'
+  $elasticsearch_rpm_version    = '1.4.1-1'
+  $user                         = 'elasticsearch'
+  $group                        = 'elasticsearch'
+  $install_dir                  = '/usr/share'
+  $config_dir                   = '/etc/elasticsearch'
+  $data_dirs                    = [ "/var/lib/elasticsearch", ]
+  $log_dir                      = '/var/log/elasticsearch'
+  $plugin_dir                   = '/usr/share/elasticsearch/plugins'
+  $work_dir                     = '/tmp/elasticsearch/work'
+  $tmp_dir                      = '/tmp/elasticsearch/tmp'
+  $number_of_shards             = '1'
+  $number_of_replicates         = '0'
+  $es_heap_size                 = '2g'
+  $indices_fielddata_cache_size = '75%' # can be secified as absolute size or percentage of heap
+  $max_open_file_descriptors    = '32768'
+  $mlockall                     = 'true'
+  $cluster_name                 = 'lumify_elasticsearch'
 
   case $architecture {
     'x86_64': {
