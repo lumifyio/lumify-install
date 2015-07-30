820f14ee7aed63932d937b184a715cdbf9752fc3
sschauer <steele.schauer@altamiracorp.com>
2015-07-30 08:40:22 -0400

    changed minimal ontology to sinaloa ontology, auth social plugin is now possible

--- puppet/modules/lumify/manifests/plugins/auth_social/deploy.pp
+++ puppet/modules/lumify/manifests/plugins/auth_social/deploy.pp
@@ -10,26 +10,9 @@ class lumify::plugins::auth_social::deploy inherits lumify {
     require => [ User['lumify'], Group['lumify'], ],
   }
 
-  macro::ensure_dir{ "auth_social_config_dir" :
-    dir     => "${lumify_config_dir}",
-    owner   => 'lumify',
-    group   => 'lumify',
-    mode    => 'u=rwx,g=rx,o=rx',
-    require => [ User['lumify'], Group['lumify'], ],
-  }
-
-  file { "${lumify_config_dir}/lumify-auth-social.properties" :
+  file { "${lumify_target_lib_dir}/lumify-web-auth-social-${lumify_version}-jar-with-dependencies.jar" :
     ensure  => file,
-    content => template('lumify/lumify-auth-social.properties.erb'),
-    require => Macro::Ensure_dir["auth_social_config_dir"],
-    owner   => 'lumify',
-    group   => 'lumify',
-    mode    => 'u=rw,go=r',
-  }
-
-  file { "${lumify_target_lib_dir}/lumify-auth-social-${lumify_version}.jar" :
-    ensure  => file,
-    source  => "${lumify_deployed_libs}/lumify-auth-social-${lumify_version}.jar",
+    source  => "${lumify_deployed_libs}/lumify-web-auth-social-${lumify_version}-jar-with-dependencies.jar",
     require => Macro::Ensure_dir["auth_social_lib_dir"],
     owner   => 'lumify',
     group   => 'lumify',
