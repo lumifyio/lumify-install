13837d1486c86a138fbb12590e93d1bf934cacab
sschauer <steele.schauer@altamiracorp.com>
2015-08-04 13:39:50 -0400

    config fix for plugins

--- puppet/modules/lumify/manifests/plugins/auth_social/deploy.pp
+++ puppet/modules/lumify/manifests/plugins/auth_social/deploy.pp
@@ -18,19 +18,19 @@ class lumify::plugins::auth_social::deploy inherits lumify {
     require => [ User['lumify'], Group['lumify'], ],
   }
 
-  file { "${lumify_target_lib_dir}/lumify-web-auth-social-${lumify_version}-jar-with-dependencies.jar" :
+  file { "${lumify_config_dir}/lumify-oauth.properties" :
     ensure  => file,
-    source  => "${lumify_deployed_libs}/lumify-web-auth-social-${lumify_version}-jar-with-dependencies.jar",
-    require => Macro::Ensure_dir["auth_social_lib_dir"],
+    content => template('lumify/lumify-oauth.properties.erb'),
+    require => Macro::Ensure_dir["auth_social_config_dir"],
     owner   => 'lumify',
     group   => 'lumify',
     mode    => 'u=rw,go=r',
   }
 
-  file { "${lumify_config_dir}/lumify-oauth.properties" :
+  file { "${lumify_target_lib_dir}/lumify-web-auth-social-${lumify_version}-jar-with-dependencies.jar" :
     ensure  => file,
-    content => template('lumify/lumify-oauth.properties.erb'),
-    require => Macro::Ensure_dir["auth_social_config_dir"],
+    source  => "${lumify_deployed_libs}/lumify-web-auth-social-${lumify_version}-jar-with-dependencies.jar",
+    require => Macro::Ensure_dir["auth_social_lib_dir"],
     owner   => 'lumify',
     group   => 'lumify',
     mode    => 'u=rw,go=r',
